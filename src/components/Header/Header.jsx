8abc85654ae8c1c1465b601324b6bd5b583dc0c9
original-Rick <74429272+original-Rick@users.noreply.github.com>
2021-11-23 01:17:56 +0300

    Added node-sass, dataMenu (array), Header, file Header.module.scss

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,16 +1,39 @@
 import React from "react";
 
+// Scss
+import styles from "./Header.module.scss";
+
+// Data
+import {dataMenu} from "./dataMenu";
+
 // Images
 import logoImage from "../../assets/images/Header/logo.svg"
 
 const Header = () => {
-  return (
-      <header>
-        <div className="logo">
-            <img src={logoImage} alt="logo" />
-        </div>
-      </header>
-  )
+    return (
+        <header className={styles.header}>
+            <div className={styles.wrapper}>
+                <div className={styles.logo}>
+                    <img src={logoImage} alt="logo"/>
+                </div>
+                <nav className={styles.nav}>
+                    <ul className={styles['nav-menu']}>
+                        {dataMenu.map((item, idx) =>
+                            (
+                                <li className={styles['nav-menu__item']} key={`key nav menu item ${idx}`}>
+                                    <a href={item.link}>{item.title}</a>
+                                </li>
+                            )
+                        )}
+                    </ul>
+                    <div className={styles['nav-buttons']}>
+                        <button className={styles['nav-buttons__button-login']}></button>
+                        <button className={styles['nav-buttons__button-sign-up']}></button>
+                    </div>
+                </nav>
+            </div>
+        </header>
+    )
 }
 
-export default  Header;
+export default Header;
---
+++ src/components/Header/dataMenu.js
@@ -0,0 +1,18 @@
+export const dataMenu = [
+    {
+        title: "Home",
+        link: "/"
+    },
+    {
+        title: "About",
+        link: "/about"
+    },
+    {
+        title: "FAQ",
+        link: "/faq"
+    },
+    {
+        title: "News",
+        link: "/news"
+    }
+]
